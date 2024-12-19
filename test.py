70cefac55ee4dae8c8db0a88d0c1c81370a8c0ca
Pol <pol.carnicer@estudiantat.upc.edu>
2024-12-19 15:19:59 +0100

    python-app.yml

--- test.py
+++ test.py
@@ -1,18 +1,35 @@
+"""
+Este módulo contiene las pruebas unitarias para el código principal.
+"""
+
 import unittest
 import transform
 
 
+
 class TestStringMethods(unittest.TestCase):
+    """
+        Esta clase contiene las pruebas unitarias para la función 'StringMethods'.
+        """
 
     def test_is_upper(self):
+        """
+                Esta función cambia un string a mayusculas.
+                """
         sting = transform.to_upper_case("hello")
         self.assertEqual(sting, "HELLO")
 
     def test_is_lower(self):
+        """
+                Esta función cambia un string a minusculas.
+                """
         sting = transform.to_lower_case("HELLO")
         self.assertEqual(sting, "hello")
 
     def test_is_capitalize(self):
+        """
+                Esta función capitaliza un string.
+                """
         sting = transform.to_capitalize("HELLO")
         self.assertEqual(sting, "Hello")
 
